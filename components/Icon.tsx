bdb7333d367f05c83881a37b1b56e72aa7c22174
Leonam Brites <102072289+leonambrites@users.noreply.github.com>
2025-10-15 14:47:19 -0300

    Update Icon.tsx

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -24,11 +24,9 @@ const Icon: React.FC<IconProps> = ({ name, className = 'w-6 h-6' }) => {
           </svg>
         );
       case 'whatsapp':
-        return <FaWhatsapp className={className} />
-        ;
+        return <FaWhatsapp className={className} />;
       case 'instagram':
-        return <FaInstagram className={className} />
-        ;
+        return <FaInstagram className={className} />;
       case 'cart':
         return (
           <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
