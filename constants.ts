f80375a6baa08a92fc48fb6a5adced8209688678
Leonam Brites <102072289+leonambrites@users.noreply.github.com>
2025-10-27 10:45:29 -0300

    feat: Add new menu items and update existing ones
    This commit introduces new menu items to the Cardápio LEBRE, including "Feijão", "Quinoa com cenoura, brócolis e pimentões", and "Molho de tomate caseiro". It also refines the description for "Bolinho de frango com queijo" to accurately reflect the quantity and adjusts the price for the "Sopa Tem tudo" 220ml size.

--- constants.ts
+++ constants.ts
@@ -15,7 +15,7 @@ export const MENU_DATA: MenuCategoryType[] = [
       {
         id: 101,
         name: 'Bolinho de frango com queijo',
-        description: '5 unidades do delicioso bolinho com massa de batata doce e frango, recheado com queijo.',
+        description: '4 unidades do delicioso bolinho com massa de batata doce e frango, recheado com queijo.',
         price: 21,
         imageUrl: 'https://i.imgur.com/Mu3eoJm.png',
         hasSaltOption: true,
@@ -38,6 +38,20 @@ export const MENU_DATA: MenuCategoryType[] = [
       },
       {
         id: 105,
+        name: 'Feijão',
+        description: 'Ingredientes: Feijão preto com tempero caseiro.',
+        price: 13,
+        imageUrl: 'https://i.imgur.com/o3xZiZY.png',
+      },
+      {
+        id: 106,
+        name: 'Quinoa com cenoura, brócolis e pimentões',
+        description: 'Ingredientes: Quioa, cenoura, brócolis, pimentão amarelo, pimentão vermelho, alho, azeite e sal.',
+        price: 13,
+        imageUrl: 'https://i.imgur.com/45GEvjw.png',
+      },
+      {
+        id: 107,
         name: 'Molho de tomate caseiro [200g]',
         description: 'Ingredientes: tomates, manjericão, cebola roxa, cenoura, beterraba, pimentão vermelho, alho, azeite e sal.',
         price: 13,
@@ -55,7 +69,7 @@ export const MENU_DATA: MenuCategoryType[] = [
         name: 'Sopa Tem tudo',
         description: 'Sopa com Frango, abóbora, grão de bico, macarrão de letrinhas, abobrinha, cenoura, vagem, batata doce e agrião.',
         sizes: [
-          { size: '220ml', price: 14 },
+          { size: '220ml', price: 10 },
           { size: '460ml', price: 18 },
         ],
         imageUrl: 'https://i.imgur.com/9NxFRfY.png',
@@ -66,7 +80,7 @@ export const MENU_DATA: MenuCategoryType[] = [
         name: 'Sopa de ervilha',
         description: 'Sopa cremosa de ervilha com carne, cenoura, batata inglesa e vagem.',
         sizes: [
-          { size: '220ml', price: 14 },
+          { size: '220ml', price: 10 },
           { size: '460ml', price: 18 },
         ],
         imageUrl: 'https://i.imgur.com/mchEq5z.png',
@@ -78,7 +92,7 @@ export const MENU_DATA: MenuCategoryType[] = [
         description: 'Sopa cremosa de batata baroa com frango desfiado, alho poró e brócolis.',
         sizes: [
           { size: '220ml', price: 14 },
-          { size: '460ml', price: 18 },
+          { size: '460ml', price: 22 },
         ],
         imageUrl: 'https://i.imgur.com/1QBTR8V.png',
         hasSaltOption: true,
@@ -94,14 +108,14 @@ export const MENU_DATA: MenuCategoryType[] = [
             id: 301,
             name: 'Cupcake de morango',
             description: '4 unidades de bolinhos fofinhos de aveia e banana com pedacinhos de morango natural.',
-            price: 19,
+            price: 20,
             imageUrl: 'https://i.imgur.com/GaPvOiG.png',
         },
         {
             id: 302,
             name: 'Cupcake de chocolate',
             description: '4 unidades do clássico cupcake de chocolate em versão sem açúcar.',
-            price: 17,
+            price: 16,
             imageUrl: 'https://i.imgur.com/MoyDbjW.png',
         },
         {
@@ -115,32 +129,67 @@ export const MENU_DATA: MenuCategoryType[] = [
   },
   {
     id: 4,
+    name: 'Bebidas',
+    subtitle: 'Naturais e sem adição de açúcar',
+    items: [
+        {
+            id: 401,
+            name: 'Summer Vibes - Suco Misto de Manga Maracujá Abacaxi e Maçã. 1l',
+            description: 'Suco Misto de Manga Maracujá Abacaxi e Maçã.',
+            price: 25,
+            imageUrl: 'https://casaspedro.vteximg.com.br/arquivos/ids/203327-550-550/suco-misto-de-manga-maracuja-abacaxi-e-maca-summer-vibes-1l.png?v=638908854656070000',
+        },
+        {
+            id: 402,
+            name: 'Greenpeople - Suco de abacaxi com hortelã 1L',
+            description: 'Um suco refrescante para qualquer ocasião. Abacaxi com hortelã é cítrico, doce e muito equilibrado. Traz o sabor inconfundível da fruta, com a fragrância inigualável do hortelã.',
+            price: 25,
+            imageUrl: 'https://trela.com.br/_next/image?url=https%3A%2F%2Fprod-zapt-images.s3.sa-east-1.amazonaws.com%2Fproducts%2Fc95668ad-9c57-44ff-b5c3-8273b26935da.png&w=3840&q=75',
+        },
+        {
+            id: 403,
+            name: 'Natural One - Suco Maçã. 180ml',
+            description: 'Sabor suave e natural do morango, adoçado com banana.',
+            price: 7.90,
+            imageUrl: 'https://prezunic.vtexassets.com/arquivos/ids/185161-1200-auto?v=638368822525430000&width=1200&height=auto&aspect=true',
+        },
+        {
+            id: 405,
+            name: 'Maguary - Suco Maçã. 150ml',
+            description: 'O Maguary Fruit Shoot é feito com suco de fruta de verdade, não possui corantes e aromas artificiais e é fonte de vitaminas B6, D, B12 e Zinco, fundamentais para uma boa nutrição infantil.',
+            price: 7.90,
+            imageUrl: 'https://casaspedro.vteximg.com.br/arquivos/ids/202769-550-550/fruit-shoot-100--suco-maca-maguary-150ml.png?v=638908775984400000',
+        },
+    ]
+  },
+  {
+    id: 5,
     name: 'Sorvetes (100ml)',
     subtitle: 'Naturais, cremosos e na medida certa',
     items: [
         {
-            id: 401,
+            id: 501,
             name: 'Chocolate',
             description: 'Sorvete cremoso de cacau, adoçado com banana.',
             price: 8,
             imageUrl: 'https://i.imgur.com/AZoRF1F.jpeg',
         },
         {
-            id: 402,
+            id: 502,
             name: 'Chocolate com Amendoim',
             description: 'A união perfeita do cacau com o amendoim.',
             price: 9,
             imageUrl: 'https://i.imgur.com/AZoRF1F.jpeg',
         },
         {
-            id: 403,
+            id: 503,
             name: 'Morango',
             description: 'Sabor suave e natural do morango, adoçado com banana.',
             price: 9,
             imageUrl: 'https://i.imgur.com/nXzXeTy.jpeg',
         },
         {
-            id: 405,
+            id: 505,
             name: 'Açaí',
             description: 'Cremoso e cheio de energia: açaí batido com banana, naturalmente doce e refrescante.',
             price: 9,
@@ -149,26 +198,26 @@ export const MENU_DATA: MenuCategoryType[] = [
     ]
   },
   {
-    id: 5,
+    id: 6,
     name: 'Festa',
     subtitle: 'Leve o sabor do Lebre para suas celebrações',
     items: [
                 {
-            id: 501,
+            id: 601,
             name: 'Bolo para festa',
             description: 'Bolo de chocolate fofinho e delicioso, perfeito para festas. Consulte opções de tamanho e cobertura.',
             price: 'A combinar',
             imageUrl: 'https://i.imgur.com/I37Jdk1.png',
         },
         {
-            id: 502,
+            id: 602,
             name: 'Lanches para festa',
             description: 'Kit festa de salgados.',
             price: 'A combinar',
             imageUrl: 'https://i.imgur.com/guNluWx.png',
         },
         {
-            id: 503,
+            id: 603,
             name: 'Cupcakes para festa',
             description: 'Kit de cupcakes para festa.',
             price: 'A combinar',
